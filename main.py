09b62caec7962f1e196691a96935e345d4dd8e88
Ra1ski <ra1almaty@gmail.com>
2022-07-02 12:21:08 +0600

    Я добавил еще пару роутов

--- main.py
+++ main.py
@@ -6,3 +6,36 @@ app = FastAPI()
 @app.get('/')
 def index():
 	return 'Hello from team app'
+
+@app.get('/about')
+def about():
+	return 'This our my start-up'
+
+
+@app.get('/team')
+def my_team():
+	team = {
+	  "Helen": {
+	    "department": "Management",
+	    "age": 21,
+	    "salary": 1500000
+	  },
+	  "Ali": {
+	    "department": "Design",
+	    "age": 25
+	  },
+	  "Katya": {
+	    "department": "IT",
+	    "age": 26
+	  },
+	  "Polina": {
+	    "department": "Designer",
+	    "age": 21,
+	    "salary": 1500000,
+	    "alumni": "NU"
+	  }
+	}
+	
+	return team
+
+#@aap.get('/team/{team}')
